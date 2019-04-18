533cc4601bb8bd8b3446a7720ae17d0c7d231df1
nguyensitien <nguyensitien481994@gmail.com>
2019-04-18 23:16:07 +0700

    Lỗi va cham

---
+++ Assets/Scripts/Controller/InputHandleClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class InputHandleClick : SingletonDontDestroy<InputHandleClick> {
+    [SerializeField]
+    private GameObject par;
+    [SerializeField]
+    private Camera cam;
+    // Use this for initialization
+	void Start () {
+        Observable.FromMicroCoroutine(UpdateClick).Subscribe().AddTo(this);
+    }
+	
+	IEnumerator UpdateClick() {
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 posMouse = Input.mousePosition;
+                posMouse = cam.ScreenToWorldPoint(posMouse);
+                GameObject obj = SmartPool.Instance.Spawn(par,Vector3.zero,Quaternion.identity);
+                obj.transform.localPosition = posMouse;
+            }
+            yield return null;
+        }
+	}
+}
